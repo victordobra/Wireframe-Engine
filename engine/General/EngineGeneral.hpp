3f31285c92cb34610ee4928cfcfa2e4bf40e424c
Victor Dobra <victordobra327@gmail.com>
2022-07-10 21:08:08 +0300

    Added engine header

---
+++ engine/General/EngineGeneral.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "GameLoop.hpp"
+#include "Input.hpp"
+#include "Time.hpp"
---
+++ engine/WireframeEngine.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "Base/Asset.hpp"
+
+#include "Assets/General/Image.hpp"
+#include "Assets/General/Model.hpp"
+#include "Assets/Rendering/Material.hpp"
+#include "Assets/Rendering/Pipeline.hpp"
+#include "Assets/Rendering/Shader.hpp"
+
+#include "ECS/EngineECS.hpp"
+
+#include "Components/Lighting/Light.hpp"
+#include "Components/Lighting/LightingSystem.hpp"
+#include "Components/Lighting/LightingUbo.hpp"
+#include "Components/Lighting/LightTypes.hpp"
+
+#include "General/EngineGeneral.hpp"
+#include "Math/EngineMath.hpp"
+#include "Vulkan/EngineVulkan.hpp"
+#include "Window/MainWindow.hpp"
