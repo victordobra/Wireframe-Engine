681d969cfe86b5b99c2075dd8bd583b21fdae355
MMG <73362859+victordobra@users.noreply.github.com>
2025-04-24 23:03:24 +0300

    Added type name getter

--- engine/WireframeEngine.hpp
+++ engine/WireframeEngine.hpp
@@ -1,6 +1,11 @@
 #pragma once
 
+#include "Core/Management/Event.hpp"
+#include "Core/Management/TypeName.hpp"
 #include "Core/Types/BuildInfo.hpp"
 #include "Core/Types/Defines.hpp"
 
 #include "Main/Program.hpp"
+
+#include "Platform/Input.hpp"
+#include "Platform/Window.hpp"
